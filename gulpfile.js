86284c6006dd1870a49a5eddeec26971dc3fc82c
michrytel <76482065+michrytel@users.noreply.github.com>
2021-05-22 23:17:29 +0200

    netlify deploy

--- gulpfile.js
+++ gulpfile.js
@@ -33,5 +33,14 @@ function reload(done) {
     browserSync.reload();
     done();
 }
+
+var netlify = require('gulp-netlify')
+gulp.task('deploy', function () {
+    gulp.src('./public/**/*')
+        .pipe(netlify({
+            site_id: "bc1f43be-d945-41d5-80e7-a268b001e766",
+            access_token: "_K1TPVQqYwV2FUdvmBva7sfdwdYIm6jCHlZkpCCD05Q"
+        }))
+})
 exports.sass = gulp.parallel(compileSass);
 exports.default = gulp.parallel(compileSass, watcher);
